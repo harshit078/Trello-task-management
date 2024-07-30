dfd33322ff49899d31d69102a687185185be899b
harshit078 <harshitsingh467@gmail.com>
2024-07-30 18:48:40 +0530

    Added SVG and Buttons component

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,28 +1,34 @@
 interface ButtonProps {
-    text: string;
-    onClick: () => void;
-    icon?: React.ReactNode;
-    loading?: boolean;
-    disabled?: boolean;
+  text: string;
+  onClick: () => void;
+  icon?: React.ReactNode;
+  loading?: boolean;
+  disabled?: boolean;
 }
 
-export const Button: React.FC<ButtonProps> = ({ text, onClick, icon, loading = false, disabled = false }) => {
-    return (
-<div className="bg-[linear-gradient(180deg,_#9C93D4_0%,_#4B36CC_100%)] rounded-md p-[1px] shadow-lg transition duration-300">
-        <button
-            onClick={disabled || loading ? undefined : onClick}
-            className="flex items-center justify-center gap-2 w-full p-2 bg-[linear-gradient(180deg,_#4C38C2_0%,_#2F2188_100%)] text-white rounded-md transition duration-300 disabled:cursor-not-allowed disabled:bg-[linear-gradient(180deg,_#9C93D4_0%,_#6f65af_100%)]"
-            disabled={disabled || loading}
-        >
-            {loading ? (
-                    <span className="loader"></span>
-                ) : (
-                    <>
-                        {text}
-                        {icon && icon}
-                    </>
-                )}
-        </button>
-        </div>
-    );
+export const Button: React.FC<ButtonProps> = ({
+  text,
+  onClick,
+  icon,
+  loading = false,
+  disabled = false,
+}) => {
+  return (
+    <div className="bg-[linear-gradient(180deg,_#9C93D4_0%,_#4B36CC_100%)] rounded-md p-[1px] shadow-lg transition duration-300">
+      <button
+        onClick={disabled || loading ? undefined : onClick}
+        className="flex items-center justify-center gap-2 w-full p-2 bg-[linear-gradient(180deg,_#4C38C2_0%,_#2F2188_100%)] text-white rounded-md transition duration-300 disabled:cursor-not-allowed disabled:bg-[linear-gradient(180deg,_#9C93D4_0%,_#6f65af_100%)]"
+        disabled={disabled || loading}
+      >
+        {loading ? (
+          <span className="loader"></span>
+        ) : (
+          <>
+            {text}
+            {icon && icon}
+          </>
+        )}
+      </button>
+    </div>
+  );
 };
---
+++ frontend/src/components/DashboardCard.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+interface DashboardCardProps {
+  imageSrc: string;
+  altText: string;
+  title: string;
+  description: string;
+}
+
+const DashboardCard: React.FC<DashboardCardProps> = ({
+  imageSrc,
+  altText,
+  title,
+  description,
+}) => {
+  return (
+    <div className="p-4 bg-white rounded-lg flex flex-row items-center gap-2">
+      <Image src={imageSrc} alt={altText} width={77} height={61} />
+      <div className="flex flex-col">
+        <h3 className="text-md text-[#757575] font-semibold">{title}</h3>
+        <p className="text-[#868686] text-sm mt-2">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardCard;
